4c89ee4fac37429e14629ee90860865612d06770
shivaconceptsolution <shivaconceptsolution@gmail.com>
2025-07-23 10:30:46 +0530

    First upload by XYZ Developer

---
+++ Healthcareproject/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Healthcareproject.Models
+{
+    public class AppDbContext : DbContext
+
+    {
+        public DbSet<Product> Products { get; set; }
+     /*   private readonly IConfiguration _configuration;
+        public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+     */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=healthcare;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
